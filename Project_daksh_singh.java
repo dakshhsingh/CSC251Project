ab8cadc4975d9f9cd2d470ea097cee973f3859bb
Daksh Singh <dakshsingh7307@gmail.com>
2026-02-09 23:49:22 -0500

    File Input
    Modified to read input from a file

---
+++ Project_daksh_singh.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+
+public class Project_daksh_singh {
+    public static void main(String[] args) throws IOException {
+        
+        ArrayList<Policy> policies = new ArrayList<>();
+
+        File file = new File("PolicyInformation.txt");
+        Scanner inputFile = new Scanner(file);
+
+        while (inputFile.hasNext()) {
+            int policyNumber = inputFile.nextInt();
+            inputFile.nextLine(); 
+            
+            String providerName = inputFile.nextLine();
+            String firstName = inputFile.nextLine();
+            String lastName = inputFile.nextLine();
+            
+            int age = inputFile.nextInt();
+            inputFile.nextLine(); 
+            
+            String smokingStatus = inputFile.nextLine();
+            double height = inputFile.nextDouble();
+            double weight = inputFile.nextDouble();
+            
+            if (inputFile.hasNext()) {
+                inputFile.nextLine(); 
+            }
+
+            Policy policy = new Policy(policyNumber, providerName, firstName, lastName, age, smokingStatus, height, weight);
+            policies.add(policy);
+        }
+        
+        inputFile.close();
+
+        for (Policy policy : policies) {
+            System.out.println("\nPolicy Number: " + policy.getPolicyNumber());
+            System.out.println("Provider Name: " + policy.getProviderName());
+            System.out.println("Policyholder's First Name: " + policy.getFirstName());
+            System.out.println("Policyholder's Last Name: " + policy.getLastName());
+            System.out.println("Policyholder's Age: " + policy.getAge());
+            System.out.println("Policyholder's Smoking Status (smoker/non-smoker): " + policy.getSmokingStatus());
+            System.out.printf("Policyholder's Height: %.1f inches\n", policy.getHeight());
+            System.out.printf("Policyholder's Weight: %.1f pounds\n", policy.getWeight());
+            System.out.printf("Policyholder's BMI: %.2f\n", policy.calculateBMI());
+            System.out.printf("Policy Price: $%.2f\n", policy.calculatePrice());
+        }
+    }
+}
